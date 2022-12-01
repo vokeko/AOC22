fee988fd126c461ddcd294adc615da2f7cded462
Voke <Vokeko@MEOWWARE>
2022-12-01 20:38:05 +0100

    Add project files.

---
+++ AOC22/Days/Day1/Day1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOC22
+{
+    static class Day1
+    {
+        internal static void CalorieCount(bool test, bool prvni)
+        {
+            string path = "data.txt";
+            if (test)
+                path = "test.txt";
+
+            path = Path.Combine(@"..\..\Days\Day1", path);
+
+            List<int> calorieList = new List<int>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                int calories = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line == "")
+                    {
+                        calorieList.Add(calories);
+                        calories = 0;
+                    }
+                    else
+                        calories += int.Parse(line);
+                }
+                if (calories != 0) calorieList.Add(calories);
+            }
+
+            if (prvni && calorieList.Count > 0)
+                Console.WriteLine(calorieList.Max());
+            else if (!prvni && calorieList.Count > 2)
+            {
+                calorieList = calorieList.OrderByDescending(x => x).ToList();
+                Console.WriteLine(calorieList[0] + calorieList[1] + calorieList[2]);
+            }
+            else
+                Console.WriteLine("Kolekce je moc malá");
+        }
+    }
+}
---
+++ AOC22/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AOC22
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Testování?");
+            string tempTestLine = Console.ReadLine().ToLower();
+            bool test = tempTestLine == "ano" || tempTestLine == "a" || tempTestLine == "y" || tempTestLine == "yes";
+
+            Console.WriteLine("První úkol?");
+            tempTestLine = Console.ReadLine().ToLower();
+            bool prvni = tempTestLine == "ano" || tempTestLine == "a" || tempTestLine == "y" || tempTestLine == "yes";
+
+            Console.WriteLine("Zadejte den");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Day1.CalorieCount(test, prvni);
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
