b59f5e86926702eb2d2c84ddc476619a13f3211f
Voke <Vokeko@MEOWWARE>
2023-01-17 08:22:15 +0100

    Day18 part 2

--- AOC22/Days/Day18/Day18.cs
+++ AOC22/Days/Day18/Day18.cs
@@ -7,7 +7,7 @@ namespace AOC22
     {
         internal static void BoilingBoulders(string path, bool prvni)
         {
-            bool[,,] grid = new bool[22, 22, 22];
+            Area[,,] grid = new Area[22, 22, 22];
             using (StreamReader sr = new StreamReader(path))
             {
                 string line;
@@ -15,23 +15,46 @@ namespace AOC22
                 while ((line = sr.ReadLine()) != null)
                 {
                     cube = Array.ConvertAll(line.Split(','), s => short.Parse(s));
-                    grid[cube[0], cube[1], cube[2]] = true;
+                    grid[cube[0], cube[1], cube[2]] = Area.Cube;
                 }
             }
 
             int sides = 0;
 
-            for (short z = 0; z <= 21; z++)
+            if (prvni)
             {
-                for (short y = 0; y <= 21; y++)
+                for (short z = 0; z <= 21; z++)
                 {
-                    for (short x = 0; x <= 21; x++)
+                    for (short y = 0; y <= 21; y++)
                     {
-                        if (grid[x, y, z] == false)
-                            continue;
-                        else
+                        for (short x = 0; x <= 21; x++)
                         {
-                            sides += CountSides(grid, x, y, z);
+                            if (grid[x, y, z] == Area.Air)
+                                continue;
+                            else
+                            {
+                                sides += CountSidesPart1(grid, x, y, z);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                SteamGrid(grid);
+
+                for (short z = 0; z <= 21; z++)
+                {
+                    for (short y = 0; y <= 21; y++)
+                    {
+                        for (short x = 0; x <= 21; x++)
+                        {
+                            if (grid[x, y, z] != Area.Cube)
+                                continue;
+                            else
+                            {
+                                sides += CountSidesPart2(grid, x, y, z);
+                            }
                         }
                     }
                 }
@@ -39,18 +62,90 @@ namespace AOC22
 
             Console.WriteLine("Stran: {0}", sides);
         }
-        private static short CountSides(bool[,,] grid, short x, short y, short z)
+
+        #region Part 1
+        private static short CountSidesPart1(Area[,,] grid, short x, short y, short z)
         {
             short sides = 0;
 
-            if (x == 0 || grid[x - 1, y, z] == false) sides++;
-            if (x == 21 || grid[x + 1, y, z] == false) sides++;
-            if (y == 0 || grid[x, y - 1, z] == false) sides++;
-            if (y == 21 || grid[x, y + 1, z] == false) sides++;
-            if (z == 0 || grid[x, y, z - 1] == false) sides++;
-            if (z == 21 || grid[x, y, z + 1] == false) sides++;
+            if (x == 0 || grid[x - 1, y, z] == Area.Air) sides++;
+            if (x == 21 || grid[x + 1, y, z] == Area.Air) sides++;
+            if (y == 0 || grid[x, y - 1, z] == Area.Air) sides++;
+            if (y == 21 || grid[x, y + 1, z] == Area.Air) sides++;
+            if (z == 0 || grid[x, y, z - 1] == Area.Air) sides++;
+            if (z == 21 || grid[x, y, z + 1] == Area.Air) sides++;
 
             return sides;
+        }
+        #endregion
+
+        #region Part 2
+        private static short CountSidesPart2(Area[,,] grid, short x, short y, short z)
+        {
+            short sides = 0;
+
+            if (x == 0 || grid[x - 1, y, z] == Area.Steam) sides++;
+            if (x == 21 || grid[x + 1, y, z] == Area.Steam) sides++;
+            if (y == 0 || grid[x, y - 1, z] == Area.Steam) sides++;
+            if (y == 21 || grid[x, y + 1, z] == Area.Steam) sides++;
+            if (z == 0 || grid[x, y, z - 1] == Area.Steam) sides++;
+            if (z == 21 || grid[x, y, z + 1] == Area.Steam) sides++;
+
+            return sides;
+        }
+
+        private static void SteamGrid(Area[,,] grid)
+        {
+            //It takes multiple iterations to steam all the viable spaces
+            for (int l = 0; l < 4; l++)
+            {
+                for (short z = 0; z <= 21; z++)
+                {
+                    for (short y = 0; y <= 21; y++)
+                    {
+                        for (short x = 0; x <= 21; x++)
+                        {
+                            if (grid[x, y, z] != Area.Air)
+                                continue;
+                            else
+                            {
+                                if (x == 0 || x == 21 || y == 0 || y == 21 || z == 0 || z == 21)
+                                    grid[x, y, z] = Area.Steam;
+                                else if (grid[x - 1, y, z] == Area.Steam || grid[x + 1, y, z] == Area.Steam || grid[x, y - 1, z] == Area.Steam || grid[x, y + 1, z] == Area.Steam || grid[x, y, z - 1] == Area.Steam || grid[x, y, z + 1] == Area.Steam)
+                                    grid[x, y, z] = Area.Steam;
+                            }
+                        }
+                    }
+                }
+
+
+                for (short z = 21; z >= 0; z--)
+                {
+                    for (short y = 21; y >= 0; y--)
+                    {
+                        for (short x = 21; x >= 0; x--)
+                        {
+                            if (grid[x, y, z] != Area.Air)
+                                continue;
+                            else
+                            {
+                                if (x == 0 || x == 21 || y == 0 || y == 21 || z == 0 || z == 21)
+                                    grid[x, y, z] = Area.Steam;
+                                else if (grid[x - 1, y, z] == Area.Steam || grid[x + 1, y, z] == Area.Steam || grid[x, y - 1, z] == Area.Steam || grid[x, y + 1, z] == Area.Steam || grid[x, y, z - 1] == Area.Steam || grid[x, y, z + 1] == Area.Steam)
+                                    grid[x, y, z] = Area.Steam;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+
+        private enum Area
+        {
+            Air,
+            Cube,
+            Steam
         }
     }
 }
